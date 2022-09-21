b09ab85d9f17aee28ffd5d8238940346f1902fbc
mr.l <admin@duxphp.com>
2022-09-21 17:15:57 +0800

    增加单选盒子样式

--- lib/form/radio.go
+++ lib/form/radio.go
@@ -7,11 +7,13 @@ import (
 type RadioOptions struct {
 	Key  any
 	Name any
+	Desc any
 }
 
 // Radio 文本输入框
 type Radio struct {
 	options []RadioOptions
+	box     bool
 }
 
 // NewRadio 创建文本
@@ -22,6 +24,12 @@ func NewRadio() *Radio {
 // SetOptions 设置选项
 func (a *Radio) SetOptions(options []RadioOptions) *Radio {
 	a.options = options
+	return a
+}
+
+// SetBox 设置盒模式
+func (a *Radio) SetBox(status bool) *Radio {
+	a.box = status
 	return a
 }
 
@@ -42,11 +50,51 @@ func (a *Radio) SaveValue(value any, data map[string]any) any {
 func (a *Radio) Render(element node.IField) *node.TNode {
 	var options []map[string]any
 	for _, item := range a.options {
-		options = append(options, map[string]any{
-			"nodeName": "a-radio",
-			"child":    item.Name,
-			"value":    item.Key,
-		})
+		if a.box {
+			options = append(options, map[string]any{
+				"nodeName": "a-radio",
+				"child": map[string]any{
+					"nodeName":    "a-space",
+					"align":       "start",
+					"class":       "custom-radio-card",
+					"vBind:class": "{ 'custom-radio-card-checked': checked }",
+					"child": []map[string]any{
+						{
+							"nodeName": "div",
+							"class":    "custom-radio-card-mask",
+							"child": map[string]any{
+								"nodeName": "div",
+								"class":    "custom-radio-card-mask-dot",
+							},
+						},
+						{
+							"nodeName": "div",
+							"child": []map[string]any{
+								{
+									"nodeName": "div",
+									"class":    "custom-radio-card-title",
+									"child":    item.Name,
+								},
+								{
+									"nodeName": "a-typography-text",
+									"type":     "secondary",
+									"child":    item.Desc,
+								},
+							},
+						},
+					},
+				},
+				"value":       item.Key,
+				"vSlot:radio": "{checked: true}",
+			})
+		} else {
+			options = append(options, map[string]any{
+				"nodeName": "a-radio",
+				"child":    item.Name,
+				"value":    item.Key,
+			})
+		}
+
 	}
 	ui := node.TNode{
 		"nodeName":          "a-radio-group",
