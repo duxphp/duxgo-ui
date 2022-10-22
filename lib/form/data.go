c52941234ac7d3ca78ed6f13e775f308fa2c7730
mr.l <admin@duxphp.com>
2022-10-22 17:55:40 +0800

    更新动态数据工具

--- lib/form/data.go
+++ lib/form/data.go
@@ -8,11 +8,17 @@ import (
 
 // Data 动态数据编辑器
 type Data struct {
-	column []map[string]any
-	option bool
-	max    uint
-	min    uint
-	wrap   bool
+	uiField []DataField
+	option  bool
+	max     uint
+	min     uint
+	wrap    bool
+}
+
+type DataField struct {
+	Name string
+	Key  string
+	UI   IElement
 }
 
 // NewData 创建日期
@@ -20,76 +26,8 @@ func NewData() *Data {
 	return &Data{}
 }
 
-func (a *Data) AddText(name string, field string, width string) *Data {
-	a.column = append(a.column, map[string]any{
-		"name":  name,
-		"key":   field,
-		"type":  "text",
-		"width": width,
-	})
-	return a
-}
-
-func (a *Data) AddSelect(name string, field string, data map[any]string, multi bool, width string) *Data {
-	a.column = append(a.column, map[string]any{
-		"name":  name,
-		"key":   field,
-		"type":  "select",
-		"multi": multi,
-		"data":  data,
-		"width": width,
-	})
-	return a
-}
-
-func (a *Data) AddImage(name string, field string, url string, width string) *Data {
-	a.column = append(a.column, map[string]any{
-		"name":  name,
-		"key":   field,
-		"type":  "image",
-		"width": width,
-		"url":   url,
-	})
-	return a
-}
-
-func (a *Data) AddArea(name string, field string, url string, multi bool, width string) *Data {
-	a.column = append(a.column, map[string]any{
-		"name":  name,
-		"key":   field,
-		"type":  "area",
-		"width": width,
-		"url":   url,
-		"multi": multi,
-	})
-	return a
-}
-
-func (a *Data) AddShow(name string, field string, width string) *Data {
-	a.column = append(a.column, map[string]any{
-		"name":  name,
-		"key":   field,
-		"type":  "show",
-		"width": width,
-	})
-	return a
-}
-
-func (a *Data) AddHtml(field string, html any) *Data {
-	a.column = append(a.column, map[string]any{
-		"key":  field,
-		"type": "html",
-		"data": html,
-	})
-	return a
-}
-
-func (a *Data) AddHidden(name string, field string) *Data {
-	a.column = append(a.column, map[string]any{
-		"name": name,
-		"key":  field,
-		"type": "show",
-	})
+func (a *Data) AddField(ui ...DataField) *Data {
+	a.uiField = ui
 	return a
 }
 
@@ -123,102 +61,31 @@ func (a *Data) SaveValue(value any, data map[string]any) any {
 	return value
 }
 
+// 数据元素
+type dataElement struct {
+	field string
+	name  string
+}
+
+func (a *dataElement) GetUIField(field ...string) string {
+	return fmt.Sprintf("value['%s']", field)
+}
+
+func (a *dataElement) GetName() string {
+	return a.name
+}
+
 // Render 渲染
 func (a *Data) Render(element node.IField) *node.TNode {
-
 	inner := []map[string]any{}
 	data := map[string]any{}
-
-	for _, column := range a.column {
-		data[column["key"].(string)] = ""
-		field := fmt.Sprintf("value['%s']", column["key"])
-		innerNode := map[string]any{}
-
-		if column["type"] == "text" {
-			innerNode = map[string]any{
-				"nodeName": "div",
-				"class":    "flex-grow",
-				"child": map[string]any{
-					"nodeName":          "a-input",
-					"placeholder":       "请输入" + column["name"].(string),
-					"vModel:modelValue": field,
-				},
-			}
-		}
-
-		if column["type"] == "image" {
-			innerNode = map[string]any{
-				"nodeName": "div",
-				"class":    "flex-none",
-				"child": map[string]any{
-					"nodeName":     "app-file",
-					"image":        true,
-					"mini":         true,
-					"size":         8,
-					"vModel:value": field,
-				},
-			}
-		}
-
-		if column["type"] == "select" {
-			options := []map[string]any{}
-			for k, v := range column["data"].(map[any]string) {
-				options = append(options, map[string]any{
-					"label": v,
-					"value": k,
-				})
-			}
-			innerNode = map[string]any{
-				"nodeName": "div",
-				"class":    "flex-grow",
-				"child": map[string]any{
-					"nodeName": "app-select",
-					"nParams": map[string]any{
-						"placeholder": "请输入" + column["name"].(string),
-						"options":     options,
-						"multiple":    column["multi"].(bool),
-					},
-					"vModel:modelValue": field,
-				},
-			}
-		}
-
-		if column["type"] == "area" {
-			innerNode = map[string]any{
-				"nodeName": "div",
-				"class":    "flex-grow",
-				"child": map[string]any{
-					"nodeName": "app-cascader",
-					"nParams": map[string]any{
-						"placeholder":  "请输入" + column["name"].(string),
-						"allow-search": true,
-						"path-mode":    true,
-						"clearable":    true,
-						"multiple":     column["multi"].(bool),
-					},
-					"dataUrl":      column["url"],
-					"vModel:value": field,
-				},
-			}
-		}
-
-		if column["type"] == "show" {
-			innerNode = map[string]any{
-				"nodeName": "div",
-				"class":    "flex-grow",
-				"child":    field,
-			}
-		}
-
-		if column["width"] != "" {
-			innerNode["style"] = map[string]any{
-				"width": column["width"],
-			}
-		}
-
-		inner = append(inner, innerNode)
+	for _, field := range a.uiField {
+		inner = append(inner, map[string]any{
+			"nodeName": "div",
+			"class":    "flex-grow",
+			"child":    field.UI.Render(&dataElement{field: fmt.Sprintf("value['%s']", field.Key), name: field.Name}),
+		})
 	}
-
 	marshal, err := json.Marshal(data)
 	if err != nil {
 		return nil
